79ed2d597020ab8bfd3f61ae1fc7ebd91523adab
Brian Illes <brianrilles@gmail.com>
2019-03-24 18:28:35 -0700

    useReducer

--- guide/src/App.js
+++ guide/src/App.js
@@ -1,14 +1,34 @@
 // Most of the examples and definitions come from the React Hooks docs which can be accessed here: https://reactjs.org/docs/hooks-intro.html
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 import Example1 from './Example1';
 import Example2 from './Example2';
+
 export default function App() {
+  const initialState = { count: 0 };
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'increment':
+        return { count: state.count + 1 };
+      case 'decrement':
+        return { count: state.count - 1 };
+      default:
+        throw new Error();
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <>
       <Example1 />
-      <p>---------------------------------------------</p>
+      <p>-------------------------------------------</p>
       <Example2 />
+      <p>-------------------------------------------</p>
+      Count: {state.count} {'  '}
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
     </>
   );
 }
